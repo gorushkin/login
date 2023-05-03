b2d4afc4c32c1e2d07ef27da9d2bb6f3b7ed3ea9
gorushkin <gorushkin@gmail.com>
2023-05-03 23:15:07 +0300

    feat: add hook useScroll

---
+++ src/Components/Pages/LoginPage/useScroll.tsx
@@ -0,0 +1,105 @@
+import { useRef, useState, CSSProperties, useLayoutEffect, RefObject } from 'react';
+
+const STEP = 50;
+const DELAY = 10;
+
+type Direction = 'left' | 'right';
+
+export const useScroll = ({
+  login,
+  page,
+  panel,
+}: {
+  login: RefObject<HTMLDivElement>;
+  panel: RefObject<HTMLDivElement>;
+  page: RefObject<HTMLDivElement>;
+}) => {
+  const isLoaded = useRef(false);
+  const isPanelClickBlocked = useRef(false);
+  const isPageUpdateBlocked = useRef(false);
+
+  const [panelStyles, setPanelStyles] = useState<CSSProperties>({});
+  const [direction, setDirection] = useState<Direction>('left');
+
+  const pageWidth = useRef<number>(0);
+  const panelWidth = useRef<number>(0);
+
+  useLayoutEffect(() => {
+    if (isLoaded.current) return;
+    const loginOffsetWidth = login.current?.offsetWidth;
+    const panelOffsetWidth = panel.current?.offsetWidth;
+    const pageOffsetWidth = page.current?.offsetWidth;
+    const styles: CSSProperties = { right: `0px`, left: 'unset' };
+    setPanelStyles(styles);
+
+    if (!panelOffsetWidth || !loginOffsetWidth || !pageOffsetWidth) return;
+
+    panelWidth.current = panelOffsetWidth;
+    pageWidth.current = pageOffsetWidth;
+    isLoaded.current = true
+  }, [login, page, panel]);
+
+  const movePanel = (currentPosition: number, currentWidth: number) => {
+    const limitPosition = pageWidth.current - panelWidth.current;
+    const newPosition = currentPosition + STEP;
+    const isLimitRightReached = newPosition >= limitPosition;
+    const position = isLimitRightReached ? limitPosition : newPosition;
+
+    const newLeftOffset = pageWidth.current - position - currentWidth;
+    const maxWidth =
+      newLeftOffset <= 0
+        ? Math.max(currentWidth + newLeftOffset, panelWidth.current)
+        : currentWidth;
+
+    const isPanelInTheMiddle = newLeftOffset <= position;
+
+    if (isPanelInTheMiddle && !isPageUpdateBlocked.current) {
+      setDirection((state) => (state === 'left' ? 'right' : 'left'));
+      isPanelClickBlocked.current = false;
+      isPageUpdateBlocked.current = true;
+    }
+
+    const right = direction === 'left' ? `${position}px` : 'unset';
+    const left = direction === 'right' ? `${position}px` : 'unset';
+
+    const styles: CSSProperties = { right, left, maxWidth: `${maxWidth}px` };
+    setPanelStyles(styles);
+    if (isLimitRightReached) return;
+    setTimeout(() => {
+      movePanel(position, maxWidth);
+    }, DELAY);
+  };
+
+  const increaseSize = (currentWidth: number) => {
+    const limitWidth = panelWidth.current * 2;
+    const newWidth = currentWidth + 50;
+    const isLimitReached = newWidth >= limitWidth;
+    const width = isLimitReached ? limitWidth : newWidth;
+    const left = direction === 'right' ? '0px' : 'unset';
+    const right = direction === 'left' ? '0px' : 'unset';
+    const styles: CSSProperties = { maxWidth: `${width}px`, left, right };
+    setPanelStyles(styles);
+    if (isLimitReached) {
+      movePanel(0, newWidth);
+      return;
+    }
+    setTimeout(() => {
+      increaseSize(width);
+    }, DELAY);
+  };
+
+  const clickPanelHandler = () => {
+    if (!panel.current || isPanelClickBlocked.current) return;
+    const { offsetWidth } = panel.current;
+    isPanelClickBlocked.current = true;
+    isPageUpdateBlocked.current = false;
+    console.log(direction);
+    const left = direction === 'right' ? '0px' : 'unset';
+    const right = direction === 'left' ? '0px' : 'unset';
+    const styles: CSSProperties = { left, right };
+    setPanelStyles(styles);
+    increaseSize(offsetWidth);
+  };
+
+  return { panelStyles, panel, clickPanelHandler, login, page, direction };
+};
