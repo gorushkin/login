c8808b1314a9eebcf42449c5a46ebaa9cda98708
gorushkin <gorushkin@gmail.com>
2023-05-03 23:19:16 +0300

    refactor: remove redundant files

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,13 +1,29 @@
-import { useCallback, useState } from 'react';
-import { LoginPage } from '../Components/Pages/LoginPage/Login';
-import { SignUpPage } from '../Components/Pages/SignUpPage/SignUpPage';
-
-export interface Page {
-  onclick: (mode: Mode) => void;
-}
-
-type Mode = 'login' | 'signUp';
+import { useRef } from 'react';
+import style from './Layout.module.scss';
+import { useScroll } from './useScroll';
 
 export const Layout = () => {
-  return <LoginPage />;
+  const panel = useRef<HTMLDivElement>(null);
+  const login = useRef<HTMLDivElement>(null);
+  const page = useRef<HTMLDivElement>(null);
+
+  const { clickPanelHandler, panelStyles, direction } = useScroll({ page, panel, login });
+
+  return (
+    <div ref={page} className={style.wrapper}>
+      {direction === 'left' && (
+        <div ref={login} className={style.login}>
+          Login
+        </div>
+      )}
+      {direction === 'right' && (
+        <div ref={login} className={style.login}>
+          signUp
+        </div>
+      )}
+      <div style={panelStyles} ref={panel} onClick={clickPanelHandler} className={style.floatPanel}>
+        Panel
+      </div>
+    </div>
+  );
 };
